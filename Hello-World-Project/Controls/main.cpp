69b54417f56c7e31ebe9b1fcd9a02f6781550496
Dominic Bilke <BilkeDominic@gmail.com>
2020-05-09 13:20:10 +0200

    Projekt Hello-World-Android Projektmappe Hello-World-Project     Projekt Hello-World-Project     Projekt Controls     Projekt Input-Datatypes-Output     Projekt Operations

--- Hello-World-Project/Controls/main.cpp
+++ Hello-World-Project/Controls/main.cpp
@@ -11,7 +11,45 @@ using namespace std;
 
 int main(int argc, const char* argv[]) {
 
+	int age;
 
+	cout << "Alter eingeben: ";
+
+	cin >> age;
+
+	if (age >= 18) {
+		cout << endl << "Du bist volljaehrig!" << endl << endl;
+	}
+	else if (age < 12) {
+		cout << endl << "Du bist noch recht jung!" << endl << endl;
+	}
+	else {
+		cout << endl << "Du bist nicht volljaehrig!" << endl << endl;
+	}
+
+
+	if (age < 16) {
+		cout << "Du darfst noch keinen Alkohol trinken!" << endl << endl;
+	}
+	else if (age >= 16 && age < 18) {
+		cout << "Du darfst Bier aber noch keine Spirituosen trinken!" << endl << endl;
+	}
+	else {
+		cout << "Du darfst Alkohol trinken!" << endl << endl;
+	}
+
+	if (age < 20) {
+
+		while (age <= 20) {
+			cout << "Du bist jetzt " << age << " Jahre alt!" << endl;
+			age++;
+		}
+
+		cout << endl << "Du bist jetzt volljaehrig!" << endl << endl;
+		cout << "Du darfst jetzt Alkohol trinken!" << endl << endl;
+	}
+
+	cout << "Dieser Block wird immer ausgefuehrt!" << endl << endl;
 
 	system("pause");
 
