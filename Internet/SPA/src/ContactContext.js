c62e5eb26f9b2bcf115f34eeec3903efc9ed4a5e
Dominic Bilke <Dominic.Bilke@outlook.com>
2023-02-06 10:59:38 +0100

    Webentwicklung: Beispiele

--- Internet/SPA/src/ContactContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from 'react';
-
-export const ContactContext = createContext(null);
-
-export const ContactProvider = (props) => {
-  const [contacts, setContacts] = useState([]);
-
-  return <ContactContext.Provider value={[contacts, setContacts]} {...props} />;
-};
